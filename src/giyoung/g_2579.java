0dbb711232461c755d64e663a49ed1e03ff331ad
niceome <gylim7826@naver.com>
2026-03-18 15:22:11 +0900

    g_2579

--- src/giyoung/g_2579.java
+++ src/giyoung/g_2579.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package giyoung;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class g_2579 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
