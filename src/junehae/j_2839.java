dfef40ccd8bf13a73297c94d155bf7bb8c238b0f
junehae <jgy06811@gmail.com>
2026-03-19 18:52:48 +0900

    2839

--- src/junehae/j_2839.java
+++ src/junehae/j_2839.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//설탕문제
 public class j_2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
